4e4ac879ba520b59a012a05ce5a256bf598cae67
Juan-Boni11a <juan_epa@outlook.com>
2024-02-02 17:54:25 -0500

    Transporte API terminado

--- src/main/java/com/example/transportsapi/TransportsApiApplication.java
+++ src/main/java/com/example/transportsapi/TransportsApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication()
+
 public class TransportsApiApplication {
 
 	public static void main(String[] args) {
